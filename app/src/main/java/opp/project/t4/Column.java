abf899e1034fb0f17d6e433c82467df1425b3e2c
txtran224 <txtran224@gmail.com>
2024-11-06 14:10:31 -0500

    added Column.java object and methods (#10)

--- app/src/main/java/opp/project/t4/Column.java
+++ app/src/main/java/opp/project/t4/Column.java
@@ -1,4 +1,33 @@
 package opp.project.t4;
+
+import java.util.ArrayList;
+
 public class Column{
+
+public final String title;
+public final ArrayList<Task> taskslist;
+
+public Column(String title){
+    this.title = title;
+    this.taskslist = new ArrayList<>();
+}
+
+public String getTitle(){
+    return title;       
+}
+public void removeTask(Task task){
+taskslist.remove(task);
+}
+public ArrayList<Task>getTasks(){
+    return taskslist;
+}
+@Override
+public String toString(){
+    StringBuilder taskslistStr = new StringBuilder();
+    for (Task task : taskslist) {
+        taskslistStr.append(task.toString()).append("\n");
+    }
+    return "Column: " +title +"\n" + taskslistStr.toString();
+}
 
 }
