9152a583786044f230b8d534ea5ace7e554d97a3
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-11-06 13:32:51 -0500

    Feature/exceptions (#8)
    * feat: added InvalidColumnException
    
    * feat: added TaskNotFoundException

---
+++ app/src/main/java/opp/project/t4/exceptions/InvalidColumnException.java
@@ -0,0 +1,6 @@
+package opp.project.t4.exceptions;
+public class InvalidColumnException extends Exception{
+    public  InvalidColumnException (String message) {
+        super(message);
+    }
+}
---
+++ app/src/main/java/opp/project/t4/exceptions/TaskNotFoundException.java
@@ -0,0 +1,6 @@
+package opp.project.t4.exceptions;
+public class TaskNotFoundException extends Exception{
+    public  TaskNotFoundException (String message) {
+        super(message);
+    }
+}
