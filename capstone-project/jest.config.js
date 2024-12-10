104c446dd9f14fa9ed4bece164fd324df13a0ad8
txtran224 <txtran224@gmail.com>
2024-12-10 11:58:34 -0500

    fix jest.config.js to allow coverage testing (#64)

--- capstone-project/jest.config.js
+++ capstone-project/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  module.exports = {
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     moduleNameMapper: {
@@ -7,4 +8,16 @@ module.exports = {
     transform: {
       "^.+\\.tsx?$": "ts-jest",
     },
+    // Add the following options:
+    collectCoverage: true, // Ensures Jest always collects coverage
+    collectCoverageFrom: [
+      "src/**/*.{js,jsx,ts,tsx}", // Specify files to include
+      "!src/**/index.ts",         // Optionally exclude files
+    ],
+    coverageDirectory: "coverage", // Where coverage reports will be stored
+    coverageReporters: [
+      "text-summary", // Outputs summary to console
+      "lcov",         // Generates lcov info for external tools
+      "html",         // Creates a human-readable HTML report
+    ],
   };
